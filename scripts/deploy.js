c248b743d38413a6d98f4d5d7da86b94efcc4141
Jagjot Singh <dev.flenex@gmail.com>
2024-01-14 23:00:38 +0530

    Initial Commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,28 +1,15 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const NAME = "NFT Generator"
+  const SYMBOL = "AINFT"
+  const COST = ethers.utils.parseUnits("1", "ether") // 1 ETH
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const NFT = await hre.ethers.getContractFactory("NFT")
+  const nft = await NFT.deploy(NAME, SYMBOL, COST)
+  await nft.deployed()
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log(`Deployed NFT Contract at: ${nft.address}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
